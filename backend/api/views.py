98b9d13a264ff75c15a359aedbe5cc600ac6a7fa
selimkocak <31386358+selimkocak@users.noreply.github.com>
2023-05-05 11:00:01 +0300

    team make api views.py update
    team make api views.py update

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from custom_user.models import Role
+from custom_user.models import Role, AppUser
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
@@ -18,60 +18,33 @@ class TaskViewSet(viewsets.ModelViewSet):
         user = self.request.user
         return Task.objects.filter(user=user)
 
-    def get_user_teams(self, user):
-        manager_teams = user.manager_teams.filter(membership__user=user)
-        member_teams = user.member_teams.filter(membership__user=user)
-        return manager_teams.union(member_teams)
-
-    def create(self, request, *args, **kwargs):
-        user_teams = self.get_user_teams(request.user)
-        team_membership_id = request.data.get('team_membership')
-
-        if team_membership_id:
-            team_membership = Team.objects.filter(pk=team_membership_id).first()
-            if team_membership and team_membership in user_teams:
-                serializer = self.get_serializer(data=request.data)
-                if serializer.is_valid():
-                    serializer.save(user=self.request.user)
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                return Response({"detail": "İzin verilmeyen takım"}, status=status.HTTP_403_FORBIDDEN)
-        else:
-            serializer = self.get_serializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save(user=self.request.user)
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
     permission_classes = [permissions.IsAuthenticated, IsTeamManager | permissions.IsAdminUser]
 
     def create(self, request, *args, **kwargs):
-        user_teams = self.get_user_teams(request.user)
-        team_membership_id = request.data.get('team_membership')
-        if team_membership_id:
-            team_membership = Membership.objects.filter(pk=team_membership_id).first()
-            if team_membership and team_membership.team in user_teams:
-                serializer = self.get_serializer(data=request.data)
-                if serializer.is_valid():
-                    serializer.save(user=self.request.user)
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-            else:
-                return Response({"detail": "İzin verilmeyen takım"}, status=status.HTTP_403_FORBIDDEN)
-        else:
-            serializer = self.get_serializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save(user=self.request.user)
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        team = serializer.save()
 
+        existing_membership = Membership.objects.filter(user=request.user, team=team).first()
+        if not existing_membership:
+            Membership.objects.create(user=request.user, team=team, role=Role.TEAM_MANAGER.value)
 
+        for member_email in request.data.get('team_members', []):
+            user = AppUser.objects.filter(email=member_email).first()
+            if user:
+                existing_membership = Membership.objects.filter(user=user, team=team).first()
+                if not existing_membership:
+                    Membership.objects.create(user=user, team=team, role=Role.TEAM_MEMBER.value)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_user_teams(self, user):
+        manager_teams = user.manager_teams.filter(membership__user=user)
+        member_teams = user.member_teams.filter(membership__user=user)
+        return manager_teams.union(member_teams)
 
     def get_permissions(self):
         if self.action == "create":
@@ -82,7 +55,6 @@ class MembershipViewSet(viewsets.ModelViewSet):
     queryset = Membership.objects.all()
     serializer_class = MembershipSerializer
     permission_classes = [permissions.IsAuthenticated, IsAdminUser]
-
 
 class UserTeamsAPIView(generics.ListAPIView):
     serializer_class = TeamSerializer
